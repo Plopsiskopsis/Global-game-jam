c6d0c82a06b3b464bc827c50f5255fe2e0df6575
Plopsis <kirvesmies70@hotmail.com>
2021-01-30 00:32:29 +0200

    Some sazzle

--- game/Levels/Level.gd
+++ game/Levels/Level.gd
@@ -1,11 +1,14 @@
 extends Node 
 
-func _ready():
+onready var tap_tween = $Tap_tween
+
+func _ready() -> void:
 	Global.level = self
 
-func _on_TextureButton_pressed():
+func _on_TextureButton_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene_to(load("res://Menu/Menu.tscn"))
 
-func tapped():
-	print("tapidi tap")
+func tapped(obj :Object) -> void:
+	tap_tween.interpolate_property(obj, "rect_position", obj.rect_position, Vector2(478.0 + rand_range(-32.0, 32.0), 300.0 + rand_range(-32.0, 32.0)), 1.0, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	tap_tween.start()
--- game/Objects/Tappable_object.gd
+++ game/Objects/Tappable_object.gd
@@ -2,12 +2,12 @@ extends TextureButton
 
 onready var anim = $AnimationPlayer
 
-func _ready():
+func _ready() -> void:
 	select_type()
 
-func _on_Tappable_object_pressed():
+func _on_Tappable_object_pressed() -> void:
 	anim.play("tapped")
-	Global.level.tapped()
+	Global.level.tapped(self)
 
-func select_type():
+func select_type() -> void:
 	modulate = Color(rand_range(0.0, 1.0), rand_range(0.0, 1.0), rand_range(0.0, 1.0), 1.0)
